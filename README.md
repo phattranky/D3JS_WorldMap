80b8d289f13423ba16fe55724c8ace26fe7b4715
Phat Tran <phattranky@gmail.com>
2016-09-01 09:03:17 +0000

    Hello World

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# D3JS_WorldMap
-WorldMap with D3JS
+# D3JS WorldMap
+
+[![dependencies](https://david-dm.org/phattranky/D3JS_WorldMap.svg)](https://david-dm.org/phattranky/D3JS_WorldMap)
+[![devDependency Status](https://david-dm.org/phattranky/D3JS_WorldMap.svg)](https://david-dm.org/phattranky/D3JS_WorldMap#info=devDependencies)
+
+##Reference Links
+
+* https://github.com/d3/d3/wiki/Tutorials
+* http://bl.ocks.org/
+* http://nicolashery.com/integrating-d3js-visualizations-in-a-react-app/
+* http://chimera.labs.oreilly.com/books/1230000000345/index.html
+* https://github.com/d3/d3/wiki/Gallery
+
+##GEO Data Resources
+* Shapefiles: http://www.diva-gis.org/gdata
+* GeoJSON: http://geojson.org
+* Topojson: https://github.com/mbostock/topojson/wiki/Command-Line-Reference
+
+## To use
+
+```sh
+npm install
+gulp lib
+npm start
+```
---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp'),
+  connect = require('gulp-connect');
+ 
+gulp.task('connect', function() {
+  connect.server({
+    root: 'app',
+    port: 8081,
+    livereload: true
+  });
+});
+ 
+gulp.task('html', function () {
+  gulp.src('./app/*')
+    .pipe(connect.reload());
+});
+
+gulp.task('lib', function () {
+  gulp.src('./node_modules/bootstrap/dist/**/*')
+  .pipe(gulp.dest('./app/libs/bootstrap'));
+  
+  gulp.src('./node_modules/d3/build/**/*')
+  .pipe(gulp.dest('./app/libs/d3/'));
+  
+  gulp.src('./node_modules/jquery/dist/**/*')
+  .pipe(gulp.dest('./app/libs/jquery'));
+  
+  gulp.src('./node_modules/topojson/build/**/*')
+  .pipe(gulp.dest('./app/libs/topojson'));
+})
+ 
+gulp.task('watch', function () {
+  gulp.watch(['./app/*.html'], ['html']);
+  gulp.watch(['./app/scripts/*.js'], ['html']);
+});
+ 
+gulp.task('default', ['connect', 'watch']);
